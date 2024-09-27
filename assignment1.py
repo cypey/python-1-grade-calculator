7d8f230c023ae2604fab997e57cf7e261cee1a4b
cypey <cyprusverbs@gmail.com>
2024-09-27 15:24:22 -0400

    actual final ver

--- assignment1.py
+++ assignment1.py
@@ -1,45 +1,44 @@
-name = input("Whats your name? ")
-print("Hello", name,"! Lets calculate figure out your grade")
+name = input("What's your name? ")
+print("Hello", name,"! Lets calculate your grade")
 
 # labs :
 labs = int(input("Enter the number of labs completed: "))
-labs_weight = 0.2
-if labs > 6:
+# labs_weight = 0.2
+if labs >6:
     labs = 6
-lab_grade = (labs*labs_weight)
+lab_grade = (((labs/6)*0.20)*100)
 
 # quizzes :
 quizzes = int(input("Enter the number of quizzes/in-class actvities completed: "))
-quizzes_weight = 0.15
+# quizzes_weight = 0.15
 if quizzes >6:
     quizzes = 6
-quiz_grade = (quizzes*quizzes_weight)
+quiz_grade = (((quizzes/6)*0.15)*100)
 
 # assignments :
 a1 = float(input("Enter your grade for Assignment 1: "))
 a2 = float(input("Enter your grade for Assignment 2: "))
 a3 = float(input("Enter your grade for Assignment 3: "))
 a4 = float(input("Enter your grade for Assignment 4: "))
-a_weight = 0.04
-a_grade = ((a1*a_weight) + (a2*a_weight) + (a3*a_weight) + (a4*a_weight))
+# a_weight = 0.04
+a_grade = ((a1*0.04) + (a2*0.04) + (a3*0.04) + (a4*0.04))
 
 # Midterms :
 mt1 = float(input("Enter your grade for Midterm 1: "))
 mt2 = float(input("Enter your grade for Midterm 2: "))
-mt_weight = 0.125
-mt_grade = ((mt1*mt_weight) + (mt2*mt_weight))
+# mt_weight = 0.125
+mt_grade = ((mt1*0.125) + (mt2*0.125))
 
 # Final :
 final = float(input("Enter your grade for the Final exam: "))
-final_weight = 0.18
-final_grade = (final*final_weight)
+# final_weight = 0.18
+final_grade = (final*0.18)
 
 # Prep :
 prep = float(input("Enter your grade for the Midterms and Final preperation: "))
-prep_weight = 0.06
-prep_grade = (prep*prep_weight)
+# prep_weight = 0.06
+prep_grade = (prep*0.06)
 
 
 grade = (lab_grade + quiz_grade + a_grade + mt_grade + final_grade + prep_grade)
 print("Your grade is:",float(round(grade,2)) ,",",name,"!")
-      
